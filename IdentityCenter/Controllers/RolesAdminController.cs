bc7cd8dc71c5ec6f4418dc8dee3e242dfbe94e04
maocaiming <maocaiming@xdf.cn>
2018-02-22 11:24:45 +0800

    Add Identity and RoleAdmin

---
+++ IdentityCenter/Controllers/RolesAdminController.cs
@@ -0,0 +1,197 @@
+﻿using IdentityCenter.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCenter.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class RolesAdminController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<ApplicationRole> _roleManager;
+        private readonly ILogger _logger;
+        public RolesAdminController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        //
+        // GET: /Roles/
+        public ActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        //
+        // GET: /Roles/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                throw new ApplicationException();
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+            // Get the list of Users in this Role
+            var users = new List<ApplicationUser>();
+
+            // Get the list of Users in this Role
+            foreach (var user in _userManager.Users.ToList())
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    users.Add(user);
+                }
+            }
+
+            ViewBag.Users = users;
+            ViewBag.UserCount = users.Count();
+            return View(role);
+        }
+
+        //
+        // GET: /Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Roles/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(ApplicationRole roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new ApplicationRole(roleViewModel.Name);
+                role.Description = roleViewModel.Description;
+                var roleresult = await _roleManager.CreateAsync(role);
+                if (!roleresult.Succeeded)
+                {
+                    AddErrors(roleresult);
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //
+        // GET: /Roles/Edit/Admin
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                throw new ApplicationException();
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                throw new ApplicationException();
+            }
+            ApplicationRole roleModel = new ApplicationRole { Id = role.Id, Name = role.Name,Description=role.Description };
+
+            return View(roleModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ApplicationRole roleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(roleModel.Id);
+                role.Name = roleModel.Name;
+                role.Description = roleModel.Description;
+
+                await _roleManager.UpdateAsync(role);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //
+        // GET: /Roles/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                throw new ApplicationException();
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                throw new ApplicationException();
+            }
+            return View(role);
+        }
+
+        //
+        // POST: /Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id, string deleteUser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == null)
+                {
+                    throw new ApplicationException();
+                }
+                var role = await _roleManager.FindByIdAsync(id);
+                if (role == null)
+                {
+                    throw new ApplicationException();
+                }
+                IdentityResult result;
+                if (deleteUser != null)
+                {
+                    result = await _roleManager.DeleteAsync(role);
+                }
+                else
+                {
+                    result = await _roleManager.DeleteAsync(role);
+                }
+                if (!result.Succeeded)
+                {
+                    AddErrors(result);
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IdentityCenter/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityCenter.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string roleName) : base(roleName) { }
+        public string Description { get; set; }
+    }
+}
