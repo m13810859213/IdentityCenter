1e8d900d1f135d6655eb8160c366876c1db23df9
mythsky <380957580@qq.com>
2018-02-23 14:53:34 +0800

    添加Hybrid授权模式

--- MvcClient/Startup.cs
+++ MvcClient/Startup.cs
@@ -24,13 +24,25 @@ namespace MvcClient
         {
             services.AddMvc();
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
+            
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = "Cookies";
                 options.DefaultChallengeScheme = "oidc";
             })
                 .AddCookie("Cookies")
+                ////implicit 模式
+                //.AddOpenIdConnect("oidc", options =>
+                //{
+                //    options.SignInScheme = "Cookies";
+
+                //    options.Authority = "http://localhost:5000";
+                //    options.RequireHttpsMetadata = false;
+
+                //    options.ClientId = "mvc";
+                //    options.SaveTokens = true;
+                //})
+                //HybridAndClientCredentials
                 .AddOpenIdConnect("oidc", options =>
                 {
                     options.SignInScheme = "Cookies";
@@ -38,9 +50,16 @@ namespace MvcClient
                     options.Authority = "http://localhost:5000";
                     options.RequireHttpsMetadata = false;
 
-                    options.ClientId = "mvc";
+                    options.ClientId = "mvchybrid";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code id_token";
+
                     options.SaveTokens = true;
-                });
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("api1");
+                    options.Scope.Add("offline_access");
+                }); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
